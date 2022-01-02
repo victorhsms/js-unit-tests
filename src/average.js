3dfd81fc42a04604bd7d17e3ff0a877f8dc4ad31
Victor Hugo <victorhsmsantos@gmail.com>
2022-01-02 08:38:40 -0300

    Iniciando o projeto. Primeiro requisito já feito! #VQV :rocket:

--- src/average.js
+++ src/average.js
@@ -11,7 +11,44 @@
     - average([1, 1]) // Retorno: 1;
     - average([1, '2']) // Retorno: undefined;
 */
+const ItsANumber = (array) => {
+  let status = 0;
 
-const average = () => {};
+  for (let i = 0; i < array.length; i += 1) {
+    if (!(Number.isInteger(array[i]))) {
+      status = undefined;
+    }
+  }
+
+  return status;
+};
+
+const program = (array) => {
+  let media = 0;
+
+  media = ItsANumber(array);
+
+  if (media === 0) {
+    for (let i = 0; i < array.length; i += 1) {
+      media += array[i];
+    }
+
+    media = Math.round(media / array.length);
+  }
+
+  return media;
+};
+
+const average = (array) => {
+  let resultado = 0;
+
+  if (array.length === 0) {
+    resultado = undefined;
+  } else {
+    resultado = program(array);
+  }
+
+  return resultado;
+};
 
 module.exports = average;
