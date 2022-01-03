f8732dc46c0987a03e9b828063794652b784fffa
Victor Hugo <victorhsmsantos@gmail.com>
2022-01-03 08:18:18 -0300

    Requisito 5 feito! :rocket:

--- src/createStudent.js
+++ src/createStudent.js
@@ -15,6 +15,11 @@
     estudante.feedback() // Retorna: 'Eita pessoa boa!'
 */
 
-const createStudent = () => {};
+const createStudent = (word) => {
+  const retorno = () => 'Eita pessoa boa!';
+  const estudante = { name: word, feedback: retorno };
+
+  return estudante;
+};
 
 module.exports = createStudent;
