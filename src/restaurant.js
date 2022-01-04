306ffe6e79b45636ca791a1141b29ea867f0995c
Victor Hugo <victorhsmsantos@gmail.com>
2022-01-04 13:38:31 -0300

    Retirei uma variável desnecessária do createMenu.pay()

--- src/restaurant.js
+++ src/restaurant.js
@@ -91,11 +91,10 @@ const createMenu = (object) => ({
   order: pedido,
   pay: () => {
     let total = 0; 
-    const itensMenu = Object.values(object); // [ {'coxinha': 3.9, 'sopa': 9.9, 'sashimi': 12.3}, {'agua': 3.9, 'cerveja': 6.9}]
     // ref: https://developer.mozilla.org/pt-BR/docs/Web/JavaScript/Reference/Global_Objects/Object/assign
-    const newItens = Object.assign(itensMenu[0], itensMenu[1]); // {'coxinha': 3.9, 'sopa': 9.9, 'sashimi': 12.3, 'agua': 3.9, 'cerveja': 6.9}
+    const itensMenu = Object.assign(object.food, object.drink); // {'coxinha': 3.9, 'sopa': 9.9, 'sashimi': 12.3, 'agua': 3.9, 'cerveja': 6.9}
     arrayAux.forEach((item) => {
-      total += newItens[item];
+      total += itensMenu[item];
     });
     
     return total * 1.1;
